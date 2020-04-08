0cbfc3dd405075628c7b28fe927cc1d90a15fdce
AnduMekon <anduabe@yahoo.com>
2020-04-08 09:57:31 -0400

    ck-3

--- src/app.js
+++ src/app.js
@@ -4,14 +4,5 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-class Ck extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1> this is ck</h1>
-            </div>
-        )
-    }
-}
 
-ReactDOM.render(<Ck />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
